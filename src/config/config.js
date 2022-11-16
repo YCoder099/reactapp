3510940e8a96054230346e31f4a4b40ca7f061d1
YCoder099 <93630425+YCoder099@users.noreply.github.com>
2022-11-15 18:32:59 -0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 global.AppConfig = {
-    serverIp: 'http://bestsellapp-env.eba-snmqqpfh.us-west-2.elasticbeanstalk.com',
+    serverIp:'http://bestsellapp-env.eba-snmqqpfh.us-west-2.elasticbeanstalk.com',
     webIp:'http://localhost:3008'
 }

5923f2d49ba5eb81bc827fef07e6b5e05f572839
YCoder099 <93630425+YCoder099@users.noreply.github.com>
2022-11-22 16:26:08 -0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 global.AppConfig = {
-    // serverIp:'https://cors-everywhere.herokuapp.com/http://bestsellapp-env.eba-snmqqpfh.us-west-2.elasticbeanstalk.com',
-    serverIp:'https://cors-everywhere.herokuapp.com/http://120.24.94.91:8090',
+    serverIp:'https://cors-everywhere.herokuapp.com/http://bestsellapp-env.eba-snmqqpfh.us-west-2.elasticbeanstalk.com',
+    // serverIp:'https://cors-everywhere.herokuapp.com/http://120.24.94.91:8090',
     webIp:'https://main.d1gvnu7kf49ils.amplifyapp.com'
 }
